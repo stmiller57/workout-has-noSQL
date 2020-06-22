35572343f0b26120969673998cda86538366b522
Reformed Scribe <stmiller57@gmail.com>
2020-06-22 17:10:16 -0400

    Implemented html routes for exercise and stats pages

--- server.js
+++ server.js
@@ -7,14 +7,19 @@ const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use(logger("dev"));
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
+
+// Requiring route paths
+require("./routes/html-routes.js")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
