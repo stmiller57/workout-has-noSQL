570bc12b296e275c3977a3f72fb705f2798efe64
Reformed Scribe <stmiller57@gmail.com>
2020-06-24 12:43:35 -0400

    Added HTML route for index page, added HTML route comments

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,9 +1,15 @@
 const router = require("express").Router();
 const path = require("path");
 
+// HTML route for main page
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+// HTML route for exercise page
 router.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 });
+// HTML route for stats page
 router.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
